a8c5a037d218c07502e4e4962a14249b0063859e
Panda-MODs <98682745+Panda-MODs@users.noreply.github.com>
2022-02-06 04:04:13 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-document.getElementById('DownloadBTN').addEventListener('click', ()=>{
+document.getElementById('FiNo_v4').addEventListener('click', ()=>{
     setTimeout(function(){
-        window.open('http://pandamods.epizy.com', '_blank');
+        window.open('https://www.mediafire.com/file/3vm90ab8q3pae03/NoteFive_V4.0_by_PandaMODS.apk/file', '_blank');
     }, 500);
 });

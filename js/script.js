f6b2ba5e49c676179aca84a00c39ec36f9fc94c0
Panda-MODs <98682745+Panda-MODs@users.noreply.github.com>
2022-02-06 22:54:33 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ document.getElementById('FiNo_v4').addEventListener('click', ()=>{
         window.open('https://www.mediafire.com/file/3vm90ab8q3pae03/NoteFive_V4.0_by_PandaMODS.apk/file', '_blank');
     }, 500);
 });
+document.getElementById('mj_n').addEventListener('click', ()=>{
+    setTimeout(function(){
+        window.open('https://mega.nz/file/NeZUWZxQ#Swc6OhDrwr2-QfJs-mWXzY5nVA-cWjZo6yKmo8_qsXw', 'blank');
+    }, 500);
+});
